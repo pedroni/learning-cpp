7a649523e53f858ea5450022f00a0f0742a0fc1b
pedroni <pedronidegodois@hotmail.com>
2025-09-07 19:44:31 -0300

    Partition list, struggling to remove the small values.
    Struggling to create a new list with the small value.
    
    This code gives some useful information head and tail of "small" and "big list".
    
    ListNode *partition(ListNode *head, int x) {
      if (head == NULL) {
        return head;
      }
    
      ListNode *temp = head;
    
      ListNode *smallHead = NULL;
      ListNode *smallTail = NULL;
    
      ListNode *bigHead = NULL;
      ListNode *bigTail = NULL;
    
      ListNode *tempNext = head->next;
      while (temp) {
        if (temp->val < x) {
          if (smallHead == NULL) {
            smallHead = temp;
            smallTail = temp;
          }
    
          if (temp->val < smallTail->val) {
            smallTail->next = temp;
            smallTail = temp;
          }
        } else {
          if (bigHead == NULL) {
            bigHead = temp;
          }
        }
    
        temp = temp->next;
      }
    
      smallTail->next = bigHead;
      int smallHeadValue = smallHead != NULL ? smallHead->val : -1;
      cout << "small head: " << smallHeadValue << endl;
    
      int smallTailValue = smallTail != NULL ? smallTail->val : -1;
      cout << "small tail: " << smallTailValue << endl;
    
      int bigHeadValue = bigHead != NULL ? bigHead->val : -1;
      cout << "big head: " << bigHeadValue << endl;
    
      return head;
    }

--- leetcode/86-partition-list.cpp
+++ leetcode/86-partition-list.cpp
@@ -1,6 +1,6 @@
-
-#include <cmath>
+#include <cstddef>
 #include <iostream>
+
 using namespace std;
 
 struct ListNode {
@@ -19,8 +19,8 @@ public:
   Solution() { this->head = NULL; }
 
   ListNode *append(int value) {
-
     ListNode *newNode = new ListNode(value);
+
     if (this->head == NULL) {
       head = newNode;
     } else {
@@ -35,38 +35,97 @@ public:
   }
 
   ListNode *partition(ListNode *head, int x) {
-    ListNode *smallHead = new ListNode();
-    return smallHead;
-  }
+    if (head == NULL) {
+      return head;
+    }
 
-  void printList() {
-    ListNode *temp = this->head;
-    while (temp) {
-      cout << temp->val;
+    ListNode *temp = head;
 
-      if (temp->next) {
-        cout << " | ";
+    ListNode *smallHead = temp->val < x ? temp : NULL;
+    ListNode *smallTail = temp->val < x ? temp : NULL;
+
+    ListNode *bigHead = temp->val >= x ? temp : NULL;
+
+    ListNode *tempNext = temp->next;
+
+    while (temp->next) {
+      tempNext = temp->next->next;
+      if (temp->next->val < x) {
+        if (smallHead == NULL) {
+          smallHead = temp->next;
+          smallHead->next = NULL;
+          smallTail = smallHead;
+
+          continue;
+        }
+        // smallTail->next = temp->next;
+        //
+        // temp->next = NULL;
+        // smallTail = temp->next;
+        //
+        temp->next = tempNext;
+
+      } else {
+        if (bigHead == NULL) {
+          bigHead = temp->next;
+        }
+      }
+
+      if (temp->next->next == NULL) {
+        //  how do i remove the last?
+        cout << "jump is null" << temp->next->val << endl;
       }
 
       temp = temp->next;
     }
-    cout << endl;
+
+    cout << "--------------------" << endl;
+    int smallHeadValue = smallHead != NULL ? smallHead->val : -1;
+    cout << "small head: " << smallHeadValue << endl;
+
+    int smallTailValue = smallTail != NULL ? smallTail->val : -1;
+    cout << "small tail: " << smallTailValue << endl;
+
+    int bigHeadValue = bigHead != NULL ? bigHead->val : -1;
+    cout << "big head: " << bigHeadValue << endl;
+
+    int headValue = head != NULL ? head->val : -1;
+    cout << "head: " << headValue << endl;
+
+    cout << "--------------------" << endl;
+    return bigHead;
   }
 };
+
+void printList(ListNode *head) {
+  ListNode *temp = head;
+  while (temp) {
+    cout << temp->val;
+
+    if (temp->next) {
+      cout << " | ";
+    }
+
+    temp = temp->next;
+  }
+  cout << endl;
+}
 
 int main() {
   Solution solution;
 
-  solution.append(3);
+  ListNode *head = solution.append(3);
   solution.append(8);
   solution.append(5);
   solution.append(10);
   solution.append(2);
   solution.append(1);
 
+  printList(head);
+
   ListNode *result = solution.partition(solution.head, 5);
 
-  solution.printList();
+  printList(result);
 
   int expectedResult = 3;
 
