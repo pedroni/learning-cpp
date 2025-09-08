2a9498527d37d040212deaaa711b1603bd324870
pedroni <pedronidegodois@hotmail.com>
2025-09-07 22:27:46 -0300

    Almost there!
    Now I'm struggling with the edge cases.
    
    input = [4,3,2,5,2] x = 3
    input = [1] x = 0
    input = [1,1] x = 0
    
    The above i was able to solve. But this one I haven't solved yet.
    input = [1,1] x = 2

--- leetcode/86-partition-list.cpp
+++ leetcode/86-partition-list.cpp
@@ -12,6 +12,20 @@ struct ListNode {
   ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+void printList(ListNode *head) {
+  ListNode *temp = head;
+  while (temp) {
+    cout << temp->val;
+
+    if (temp->next) {
+      cout << " | ";
+    }
+
+    temp = temp->next;
+  }
+  cout << endl;
+}
+
 class Solution {
 
 public:
@@ -34,8 +48,16 @@ public:
     return newNode;
   }
 
+  // the edge cases are killing me.
+  // input: [1,1] x: 0 output: [1,1]
+  // input: [1,1] x: 2 output: [1,1]
   ListNode *partition(ListNode *head, int x) {
     if (head == NULL) {
+      return head;
+    }
+
+    if (head->next == NULL) {
+      // if only one item always return it
       return head;
     }
 
@@ -53,27 +75,35 @@ public:
       if (temp->next->val < x) {
         if (smallHead == NULL) {
           smallHead = temp->next;
-          smallHead->next = NULL;
           smallTail = smallHead;
-
-          continue;
         }
-        // smallTail->next = temp->next;
-        //
-        // temp->next = NULL;
-        // smallTail = temp->next;
-        //
-        temp->next = tempNext;
 
+        smallTail->next = temp->next;
+        smallTail = temp->next;
+
+        temp->next = tempNext;
+        smallTail->next = NULL;
+
+        // continue iteration so that we don't change `temp` this is necessary
+        // so we can continue looking forward. without skipping the current item
+        // the current item in this case is always bigger than the next value.
+        //
+        // say we had: x = 5, temp = 10
+        // temp->next = 2 // this needs to become 1, bcs its lower than 5
+        // temp->next->next = 1
+        //
+        // and on the following iteration we will have
+        // temp = 10
+        // temp->next = 1
+        // which will allow us to work on the 1 item and will give the ability
+        // to remove the next pointer of 10. if we were to change temp to
+        // temp->next then we'd skip working on 10, and there could be a skipped
+        // value, this is better seen in the whiteboard.
+        continue;
       } else {
         if (bigHead == NULL) {
           bigHead = temp->next;
         }
-      }
-
-      if (temp->next->next == NULL) {
-        //  how do i remove the last?
-        cout << "jump is null" << temp->next->val << endl;
       }
 
       temp = temp->next;
@@ -93,44 +123,51 @@ public:
     cout << "head: " << headValue << endl;
 
     cout << "--------------------" << endl;
-    return bigHead;
-  }
-};
 
-void printList(ListNode *head) {
-  ListNode *temp = head;
-  while (temp) {
-    cout << temp->val;
-
-    if (temp->next) {
-      cout << " | ";
+    if (smallTail == NULL) {
+      // when there's no small tail it means all items were bigger than x and we
+      // do not have a small list
+      return bigHead;
     }
 
-    temp = temp->next;
+    // merge both partitions, the small at the start and the big at the end!
+    smallTail->next = bigHead;
+
+    // then we return the smallHead which is the newest head that contains
+    return smallHead;
   }
-  cout << endl;
-}
+};
 
 int main() {
   Solution solution;
 
-  ListNode *head = solution.append(3);
-  solution.append(8);
-  solution.append(5);
-  solution.append(10);
-  solution.append(2);
+  // ListNode *head = solution.append(3);
+  // solution.append(8);
+  // solution.append(5);
+  // solution.append(10);
+  // solution.append(2);
+  // solution.append(1);
+
+  ListNode *head = solution.append(0);
   solution.append(1);
+  // solution.append(2);
+  // solution.append(5);
+  // solution.append(2);
 
   printList(head);
 
-  ListNode *result = solution.partition(solution.head, 5);
+  ListNode *result = solution.partition(solution.head, 2);
 
   printList(result);
 
   int expectedResult = 3;
 
   cout << "Expected: " << expectedResult << endl;
-  cout << "Got: " << result->val << endl;
-  cout << "Test " << (result->val == expectedResult ? "✅ Passed" : "❌ Failed")
-       << endl;
+  if (result != NULL) {
+    cout << "Got: " << result->val << endl;
+    cout << "Test "
+         << (result->val == expectedResult ? "✅ Passed" : "❌ Failed") << endl;
+  } else {
+    cout << "Result is NULL" << endl;
+  }
 }
